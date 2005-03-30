ea94c1f9d222953f81c0e6a88655f9c2c25887ef
Fabrice Bacchella <fbacchella@spamcop.net>
2005-03-30 16:39:50 +0000

    Streamlined JrdsLogger

--- src/jrds/JrdsLogger.java
+++ src/jrds/JrdsLogger.java
@@ -6,7 +6,6 @@
 package jrds;
 
 import java.io.IOException;
-import java.util.Properties;
 
 import org.apache.log4j.Appender;
 import org.apache.log4j.ConsoleAppender;
@@ -23,28 +22,23 @@ import org.apache.log4j.PropertyConfigurator;
  *  @author Fabrice Bacchella
  */
 public class JrdsLogger {
-	static private final String APPENDER = "JRDS";
+	static private final String APPENDER = "jrds";
 	static private final Level DEFAULTLEVEL = Level.DEBUG;
-	private static PropertiesManager pm;
+	static private final String DEFAULTLOGFILE = ConsoleAppender.SYSTEM_ERR;
 	static {
-		Properties log4jprop = new Properties();
-		
-		Appender app = new ConsoleAppender(new org.apache.log4j.SimpleLayout(), ConsoleAppender.SYSTEM_ERR);
-		app.setName(APPENDER);
-		Logger logger = Logger.getLogger(JrdsLogger.class.getPackage().getName());
-		logger.addAppender(app);
-		logger.setLevel(DEFAULTLEVEL);
-		
-		Logger snmp4jLogger = Logger.getLogger("org.snmp4j");
-		snmp4jLogger.setLevel(Level.WARN);
-		snmp4jLogger.addAppender(app);
-		
-		pm = PropertiesManager.getInstance();
-		logger.setLevel(pm.loglevel);
-		PropertyConfigurator.configure(pm.getProperties());
+		try {
+			Appender app = getAppender(false, DEFAULTLOGFILE);
+			initLog(app);
+		} catch (IOException e) {
+			Logger.getLogger(".").error("Unable to open " + DEFAULTLOGFILE + ": " + e.getLocalizedMessage());
+		}
 	}
 
+	/**
+	 * Private because all the method of the object are static
+	 */
 	private JrdsLogger() {
+		
 	}
 	
 	/**
@@ -52,7 +46,7 @@ public class JrdsLogger {
 	 * @param clazz the class of the objet to get a logger for
 	 * @return configured logger
 	 */
-	static public org.apache.log4j.Logger getLogger(Class clazz){
+	static final public org.apache.log4j.Logger getLogger(Class clazz){
 		return Logger.getLogger(clazz.getPackage().getName());
 	}
 
@@ -61,32 +55,49 @@ public class JrdsLogger {
 	 * @param clazz the appender name
 	 * @return configured logger
 	 */
-	static public org.apache.log4j.Logger getLogger(String clazz){
+	static final public org.apache.log4j.Logger getLogger(String clazz){
 		return Logger.getLogger(clazz);
 	}
 	
-	/** Configure loggin to run in a web application
-	 * the pat to the logfile is set by the propertie logile
+	/**
+	 * Configure logging to go to a file
+	 * @param logfile The file to send log to
 	 */
-	static public void doEmbeded()
+	static final public void setFileLogger(String logfile)
 	{
-		
-		Logger logger = Logger.getLogger(JrdsLogger.class.getPackage().getName());
 		try {
-			Layout l = new PatternLayout("[%d] %5p %c : %m%n");
-			Appender app = new DailyRollingFileAppender(new org.apache.log4j.SimpleLayout(), pm.logfile,
-						"'.'yyyy-ww");
-			app.setLayout(l);
-			app.setName(APPENDER);
-			logger.removeAppender(APPENDER);
-			logger.addAppender(app);
-
-			logger.setLevel(pm.loglevel);
-			PropertyConfigurator.configure(pm.getProperties());
+			Appender app = getAppender(true, logfile);
+			initLog(app);
 		} catch (IOException e) {
-			logger.error("Unable to open logfile " + pm.logfile + ": " + e.getLocalizedMessage());
+			Logger.getLogger(".").error("Unable to open " + logfile + ": " + e.getLocalizedMessage());
 		}
+	}
+	
+	static final private void initLog(Appender app) {
+		Logger logger = Logger.getLogger(APPENDER);
+		logger.removeAppender(APPENDER);
+		logger.addAppender(app);
 		
+		Logger snmp4jLogger = Logger.getLogger("org.snmp4j");
+		snmp4jLogger.setLevel(Level.WARN);
+		snmp4jLogger.addAppender(app);
+		
+		PropertiesManager pm = PropertiesManager.getInstance();
+		logger.setLevel(pm.loglevel);
+		PropertyConfigurator.configure(pm.getProperties());
+	}
+	
+	static final private Appender getAppender(boolean isFile, String logfile) throws IOException {
+		Appender newApp = null;
+		if(isFile) {
+			Layout l = new PatternLayout("[%d] %5p %c : %m%n");
+			newApp = new DailyRollingFileAppender(l, logfile, "'.'yyyy-ww");
+		}
+		else {
+			newApp = new ConsoleAppender(new org.apache.log4j.SimpleLayout(), logfile);
+		}
+		newApp.setName(APPENDER);			
+		return newApp;
 	}
 
 }
