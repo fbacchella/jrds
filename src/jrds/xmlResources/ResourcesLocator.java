654477e126f667c0db1570d21929dfbbd7a34ef2
Fabrice Bacchella <fbacchella@spamcop.net>
2005-04-09 23:11:21 +0000

    Adding debuging information in the log

--- src/jrds/xmlResources/ResourcesLocator.java
+++ src/jrds/xmlResources/ResourcesLocator.java
@@ -5,6 +5,9 @@ package jrds.xmlResources;
 
 
 import java.io.InputStream;
+import jrds.RdsGraph;
+import jrds.JrdsLogger;
+import org.apache.log4j.Logger;
 
 /**
  * A almost empty class, just used to return resources stored in the same package.
@@ -12,27 +15,30 @@ import java.io.InputStream;
  * @version $Revision$
  */
 public class ResourcesLocator {
-	/**
-	 * We don't want any constructor
-	 */
-	private ResourcesLocator() {};
-	
-	/**
-	 * Return the URL of any resource stored in the same package
-	 * @param name
-	 * @return
-	 */
-	public static final java.net.URL getResourceUrl(String name) {
-		return ResourcesLocator.class.getResource(name);
-	}
-	
-	/**
-	 * Return a resource stored in the same package as a InputStream
-	 * @param name
-	 * @return
-	 */
-	public static final InputStream getResource(String name) {
-		return ResourcesLocator.class.getResourceAsStream(name);
-	}
+    static final private Logger logger = JrdsLogger.getLogger(RdsGraph.class);
+    /**
+     * We don't want any constructor
+     */
+    private ResourcesLocator() {};
+
+    /**
+     * Return the URL of any resource stored in the same package
+     * @param name
+     * @return
+     */
+    public static final java.net.URL getResourceUrl(String name) {
+
+        return ResourcesLocator.class.getResource(name);
+    }
+
+    /**
+     * Return a resource stored in the same package as a InputStream
+     * @param name
+     * @return
+     */
+    public static final InputStream getResource(String name) {
+        logger.debug("URL for " + name + ": " + getResourceUrl(name));
+        return ResourcesLocator.class.getResourceAsStream(name);
+    }
 
 }
