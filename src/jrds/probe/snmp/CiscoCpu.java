2243b6bc555d05cc5d9896a97016375e3ed490be
Fabrice Bacchella <fbacchella@spamcop.net>
2005-09-10 19:14:36 +0000

    Graph is now an xml

--- src/jrds/probe/snmp/CiscoCpu.java
+++ src/jrds/probe/snmp/CiscoCpu.java
@@ -6,12 +6,9 @@ package jrds.probe.snmp;
 import jrds.ProbeDesc;
 import jrds.RdsHost;
 import jrds.RdsSnmpSimple;
-
-import org.snmp4j.smi.OID;
-import jrds.GraphDesc;
 import jrds.snmp.SnmpRequester;
 
-import java.awt.Color;
+import org.snmp4j.smi.OID;
 
 
 /**
@@ -20,30 +17,22 @@ import java.awt.Color;
  * @version $Revision$
  */
 public class CiscoCpu extends RdsSnmpSimple {
-    static final private ProbeDesc pd = new ProbeDesc(2);
-    static {
-        pd.add("la1", ProbeDesc.GAUGE, new OID(".1.3.6.1.4.1.9.2.1.57"));
-        pd.add("la5", ProbeDesc.GAUGE, new OID(".1.3.6.1.4.1.9.2.1.58"));
-        pd.setRrdName("ciscocpuload");
-        pd.setRequester(SnmpRequester.SIMPLE);
-
-        GraphDesc gd = new GraphDesc(2);
-        gd.add("la1", GraphDesc.LINE, Color.GREEN, "1mn");
-        gd.add("la5", GraphDesc.LINE, Color.BLUE, "5mn");
-        gd.setGraphName("ciscocpu");
-        gd.setGraphName("Charge CPU");
-        gd.setHostTree(GraphDesc.HSLT);
-        gd.setViewTree(GraphDesc.SLHT);
-
-        pd.setGraphClasses(new Object[] {gd});
-    }
-
-    /**
-     * A basic, default constructor
-     * @param monitoredHost
-     */
-    public CiscoCpu(RdsHost monitoredHost) {
-        super(monitoredHost, pd);
-    }
-
+	static final private ProbeDesc pd = new ProbeDesc(2);
+	static {
+		pd.add("la1", ProbeDesc.GAUGE, new OID(".1.3.6.1.4.1.9.2.1.57"));
+		pd.add("la5", ProbeDesc.GAUGE, new OID(".1.3.6.1.4.1.9.2.1.58"));
+		pd.setRrdName("ciscocpuload");
+		pd.setRequester(SnmpRequester.SIMPLE);
+		
+		pd.setGraphClasses(new Object[] {"cpuload.xml"});
+	}
+	
+	/**
+	 * A basic, default constructor
+	 * @param monitoredHost
+	 */
+	public CiscoCpu(RdsHost monitoredHost) {
+		super(monitoredHost, pd);
+	}
+	
 }
