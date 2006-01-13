1989afc85d633c7895c705f9cfd43e046ca8fa83
Fabrice Bacchella <fbacchella@spamcop.net>
2006-01-13 21:25:00 +0000

    Graph modified

--- src/jrds/graphe/PagingSolarisMuninsGraph.java
+++ src/jrds/graphe/PagingSolarisMuninsGraph.java
@@ -19,14 +19,14 @@ public class PagingSolarisMuninsGraph extends RdsGraph {
 	static final GraphDesc gd = new GraphDesc(7);
 	static {
 		gd.setGraphName("pagesolmunins");
-		gd.setGraphName("Paging activity");
+		gd.setGraphTitle("Paging activity");
 		gd.setVerticalLabel("operation/s");
 		gd.add(GraphDesc.COMMENT,"Upward graph");
 		gd.add("pgin",GraphDesc.LINE, "Page-in requests");
 		gd.add("pgpgin",GraphDesc.LINE, "pages paged-in");
-		gd.add("reclaim",GraphDesc.LINE, "Pages reclaimed");
+		//gd.add("reclaim",GraphDesc.LINE, "Pages reclaimed");
 		gd.colorsReset();
-		gd.add(GraphDesc.COMMENT,"Downard graph");
+		gd.add(GraphDesc.COMMENT,"Downward graph");
 		gd.add("pgout");
 		gd.add("scan");
 		gd.add("pgpgout");
@@ -34,7 +34,7 @@ public class PagingSolarisMuninsGraph extends RdsGraph {
 		gd.add("rpgout","0, pgout, - ", GraphDesc.LINE, "page-out requests");
 		gd.add("rpgpgout","0, scan, - ", GraphDesc.LINE, "pages paged-out");
 		gd.add("rscan","0, scan, - ", GraphDesc.LINE, "pages per second scanned by");
-		gd.add("rpgfree","0, pgfree, - ", GraphDesc.LINE, "pages per second placed on the free list");
+		//gd.add("rpgfree","0, pgfree, - ", GraphDesc.LINE, "pages per second placed on the free list");
 		gd.setHostTree(new Object[] { GraphDesc.HOST, GraphDesc.SYSTEM, GraphDesc.MEMORY, GraphDesc.TITLE});
 		gd.setViewTree(new Object[] { GraphDesc.SYSTEM, GraphDesc.MEMORY, GraphDesc.TITLE, GraphDesc.HOST, });
 	}
