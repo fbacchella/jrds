bbd48baf700cca5bd45c51fe1b238b5dd17d96c5
Fabrice Bacchella <fbacchella@spamcop.net>
2006-01-13 21:32:58 +0000

    new probes

---
+++ src/jrds/probe/munins/MemSolaris9Munins.java
@@ -0,0 +1,52 @@
+/*
+ * Created on 11 janv. 2005
+ *
+ * TODO 
+ */
+package jrds.probe.munins;
+
+import jrds.JrdsLogger;
+import jrds.MuninsProbe;
+import jrds.ProbeDesc;
+import jrds.RdsHost;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * @author bacchell
+ *
+ * TODO 
+ */
+public class MemSolaris9Munins extends MuninsProbe {
+	static private final Logger logger = JrdsLogger.getLogger(MemSolaris9Munins.class.getPackage().getName());
+
+	static final private ProbeDesc pd = new ProbeDesc(4);
+	static {
+		
+		pd.add("swap_free", ProbeDesc.GAUGE, "swap_free.value");
+		pd.add("swap_resv", ProbeDesc.GAUGE, "swap_resv.value");
+		pd.add("swap_avail", ProbeDesc.GAUGE, "swap_avail.value");
+		pd.add("swap_alloc", ProbeDesc.GAUGE, "swap_alloc.value");
+		pd.add("ram_total", ProbeDesc.GAUGE, "ram_total.value");
+		pd.add("freemem", ProbeDesc.GAUGE, "freemem.value");
+		pd.add("availrmem", ProbeDesc.GAUGE, "availrmem.value");
+		pd.add("pp_kernel", ProbeDesc.GAUGE, "pp_kernel.value");
+		pd.add("pageslocked", ProbeDesc.GAUGE, "pageslocked.value");
+		pd.add("pagestotal", ProbeDesc.GAUGE, "pagestotal.value");
+		pd.add("physmem", ProbeDesc.GAUGE, "physmem.value");
+		pd.add("swapfs_minfree", ProbeDesc.GAUGE, "swapfs_minfree.value");
+		pd.add("disk_total", ProbeDesc.GAUGE, "disk_total.value");
+		pd.add("disk_free", ProbeDesc.GAUGE, "disk_free.value");
+		pd.setRrdName("memsolaris9munins");
+		pd.setMuninsProbesNames(new String[] { "memorysolaris9"});
+		pd.setGraphClasses(new Object[] {/*"memorysolaris9.xml", "swapmemorysolaris9.xml",*/ "physicalmemorysolaris9.xml"});
+	}
+	
+	/**
+	 * @param monitoredHost
+	 */
+	public MemSolaris9Munins(RdsHost monitoredHost) {
+		super(monitoredHost, pd);
+	}
+}
