70ab03068182785c0f198cefce3d95fc9d60352b
Fabrice Bacchella <fbacchella@spamcop.net>
2006-02-09 13:11:52 +0000

    Bad placement in the tree

--- src/jrds/graphe/SwapIO.java
+++ src/jrds/graphe/SwapIO.java
@@ -22,11 +22,11 @@ public class SwapIO extends RdsGraph {
 	private static final GraphDesc gd = new GraphDesc(3);
 	static {
 		gd.setGraphName("swapio");
-		gd.setHostTree(new Object[] {GraphDesc.HOST, GraphDesc.MEMORY, GraphDesc.TITLE});
+		gd.setHostTree(new Object[] {GraphDesc.HOST,GraphDesc.SYSTEM,  GraphDesc.MEMORY, GraphDesc.TITLE});
 		gd.setViewTree(new Object[] {GraphDesc.SYSTEM, GraphDesc.MEMORY, GraphDesc.TITLE, GraphDesc.HOST});
-		gd.add("swapIn", GraphDesc.AREA, Color.BLUE);
+		gd.add("swapIn", GraphDesc.AREA, Color.BLUE, "Swap in");
 		gd.add("swapOut");
-		gd.add("swapOutInverser","0,swapOut,-", GraphDesc.AREA, Color.GREEN);
+		gd.add("swapOutInverser","0,swapOut,-", GraphDesc.AREA, Color.GREEN, "Swap out");
 		gd.setGraphName("Swap activity");
 	}
 
