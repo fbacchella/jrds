97d8b8be96dde20a3c977cdbe45581bb16f35abc
Fabrice Bacchella <fbacchella@spamcop.net>
2006-08-20 11:27:41 +0000

    Added mainly for ProcessInfoExtended

---
+++ src/jrds/snmp/TabularIterator.java
@@ -0,0 +1,43 @@
+package jrds.snmp;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import jrds.probe.snmp.SnmpProbe;
+
+import org.snmp4j.Target;
+import org.snmp4j.smi.OID;
+import org.snmp4j.util.DefaultPDUFactory;
+import org.snmp4j.util.TableEvent;
+import org.snmp4j.util.TableUtils;
+
+public class TabularIterator implements Iterator {
+	Iterator tabIterator;
+
+	public TabularIterator(SnmpProbe p, Collection<OID> oids) {
+		SnmpStarter starter = p.getSnmpStarter();
+		if(starter != null && starter.isStarted()) {
+			Target snmpTarget = starter.getTarget();
+			if(snmpTarget != null) {
+				DefaultPDUFactory localfactory = new DefaultPDUFactory();
+				TableUtils tableRet = new TableUtils(starter.getSnmp(), localfactory);
+				tableRet.setMaxNumColumnsPerPDU(30);
+				OID[] oidTab= new OID[oids.size()];
+				oids.toArray(oidTab);
+				tabIterator = tableRet.getTable(snmpTarget, oidTab, null, null).iterator();
+			}
+		}
+	}
+	public boolean hasNext() {
+		return tabIterator.hasNext();
+	}
+
+	public SnmpVars next() {
+		TableEvent te = (TableEvent) tabIterator.next();
+		SnmpVars vars = new SnmpVars(te.getColumns());
+		return vars;
+	}
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+}
