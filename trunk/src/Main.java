cbc5bb8b6419582815a11fcf963cd92e77e20763
Bruno Bonfils <bbonfils@gmail.com>
2009-06-23 05:14:16 +0000

    - Add Main.java (main class when war is used as jar file) - Add (war) manifest.mf - Update build.xml to include both

---
+++ trunk/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author bbonfils
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
