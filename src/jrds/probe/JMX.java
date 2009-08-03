9f2523d23d16b45d6dffd39fe8fcdc7a3c51570f
Fabrice Bacchella <fbacchella@spamcop.net>
2009-08-03 08:14:12 +0000

    JMX probes and the first one : JVM memory usage

---
+++ src/jrds/probe/JMX.java
@@ -0,0 +1,123 @@
+package jrds.probe;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.TabularData;
+
+import org.apache.log4j.Logger;
+
+import jrds.Connection;
+import jrds.Probe;
+
+public class JMX extends Probe {
+
+	static final private Logger logger = Logger.getLogger(JMX.class);
+
+	Connection jmxCnx = null;
+	@Override
+	public Map<?, ?> getNewSampleValues() {
+		JMXConnection cnx = (JMXConnection) getStarters().find(JMXConnection.class.getName());
+		MBeanServerConnection mbean = (MBeanServerConnection) cnx.getConnection();
+		//Uptime is collected only once, by the connexion
+		setUptime(cnx.getUptime());
+
+		try {
+
+			Map<String, String> collectKeys = getPd().getCollectStrings();
+			Map<String, Double> retValues = new HashMap<String, Double>(collectKeys.size());
+
+			logger.debug(collectKeys);
+			for(Map.Entry<String, String> e: collectKeys.entrySet()) {
+				try {
+					String[] jmxPathArray = e.getKey().split("/");
+					List<String> jmxPath = new ArrayList<String>();
+					jmxPath.addAll(Arrays.asList(jmxPathArray));
+					ObjectName mbeanName = new ObjectName(jmxPath.remove(0));
+					String attributeName =  jmxPath.remove(0);
+					Object attr = mbean.getAttribute(mbeanName, attributeName);
+					Number v = resolvJmxObject(jmxPath, attr);
+					logger.debug("JMX Path: " + e.getKey() +" = " + v);
+					retValues.put(e.getValue(), v.doubleValue());
+				} catch (AttributeNotFoundException e1) {
+					logger.error("JMX error for " + this + ": ");
+				} catch (InstanceNotFoundException e1) {
+					logger.error("JMX error for " + this + ": ");
+				} catch (MBeanException e1) {
+					logger.error("JMX error for " + this + ": ");
+				} catch (ReflectionException e1) {
+					logger.error("JMX error for " + this + ": ");
+				} catch (IOException e1) {
+					logger.error("JMX error for " + this + ": ");
+				}
+			}
+			return retValues;
+		} catch (MalformedObjectNameException e) {
+			logger.error("JMX error for " + this + ": ");
+		} catch (NullPointerException e) {
+			logger.error("JMX error for " + this + ": ");
+		}
+
+		return Collections.emptyMap();
+	}
+
+	@Override
+	public String getSourceType() {
+		return "JMX";
+	}
+
+	/**
+	 * Try to extract a numerical value from a jmx Path pointing to a jmx object
+	 * If the attribute (element 0) of the path is a :
+	 * - Set, array or TabularData, the size is used
+	 * - Map, the second element is the key to the value
+	 * - CompositeData, the second element is the key to the value
+	 * @param jmxPath
+	 * @param o
+	 * @return
+	 */
+	Number resolvJmxObject(List<String> jmxPath, Object o) {
+		Object value = null;
+		String subKey = jmxPath.remove(0);
+		if(o instanceof CompositeData) {
+			CompositeData co = (CompositeData) o;
+			value = co.get(subKey);
+		}
+		else if(o instanceof Number)
+			return (Number) o;
+		else if(o instanceof Map) {
+			value = ((Map<?, ?>) o).get(subKey);
+		}
+		else if(o instanceof Collection) {
+			return ((Collection<?>) o ).size();
+		}
+		else if(o instanceof TabularData) {
+			return ((TabularData) o).size();
+		}
+		else if(o.getClass().isArray()) {
+			return Array.getLength(o);
+		}
+		if(value instanceof Number) {
+			return ((Number) value);
+		}
+		else if(value instanceof String) {
+			return jrds.Util.parseStringNumber((String) value, Double.class, Double.NaN);
+		}
+		return Double.NaN;
+	}
+}
---
+++ src/jrds/probe/JMXConnection.java
@@ -0,0 +1,111 @@
+package jrds.probe;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+import jrds.Connection;
+
+import org.apache.log4j.Logger;
+
+public class JMXConnection extends Connection {
+	static final private Logger logger = Logger.getLogger(JMXConnection.class);
+
+	final static String startTimeObjectName = "java.lang:type=Runtime";
+	final static String startTimeAttribue = "Uptime";
+
+	private int port;
+	private String user = null;
+	private String password = null;
+	private JMXConnector connector;
+	private MBeanServerConnection connection;
+
+
+	public JMXConnection(Integer port) {
+		super();
+		this.port = port;
+	}
+
+	public JMXConnection(Integer port, String user, String password) {
+		super();
+		this.port = port;
+		this.user = user;
+		this.password = password;
+	}
+
+	@Override
+	public Object getConnection() {
+		return connection;
+	}
+
+	@Override
+	public long setUptime() {
+		ObjectName objectname;
+		try {
+			objectname = new ObjectName(startTimeObjectName);
+			Object o = connection.getAttribute(objectname, startTimeAttribue);
+			long uptime = ((Number)o).longValue() / 1000;
+			logger.debug("Uptime for " + this + "="  + uptime);
+			return uptime;
+		} catch (Exception e) {
+			logger.error("Uptime error for " + this + ": " + e);
+
+		}
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see jrds.Starter#start()
+	 */
+	@Override
+	public boolean startConnection() {
+		String uri = "service:jmx:rmi:///jndi/rmi://" + getHostName() + ":" + port + "/jmxrmi";
+		try {
+			JMXServiceURL jmxserviceurl = new JMXServiceURL(uri);
+			Map<String, Object> attributes = null;
+			if(user != null && password != null ) {
+				String[] credentials = new String[]{user, password};
+				attributes = new HashMap<String, Object>();
+				attributes.put("jmx.remote.credentials", credentials);
+			}
+			connector = JMXConnectorFactory.connect(jmxserviceurl, attributes);
+			connection = connector.getMBeanServerConnection();
+			return true;
+		} catch (MalformedURLException e) {
+			logger.error("Invalid jmx URL " + uri + ": " + e);
+		} catch (IOException e) {
+			logger.error("Communication error with " + uri + ": " + e);
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see jrds.Starter#stop()
+	 */
+	@Override
+	public void stopConnection() {
+		try {
+			connector.close();
+		} catch (IOException e) {
+			logger.error("JMXConnector to " + this + " close failed because of: " + e );
+		}
+		connection = null;
+	}
+
+	/* (non-Javadoc)
+	 * @see jrds.Starter#toString()
+	 */
+	@Override
+	public String toString() {
+		return "service:jmx:rmi:///jndi/rmi://" + getHostName() + ":" + port + "/jmxrmi";
+	}
+
+}
