d9be343ce45ec97edcee2c51846aa4ca5747a15e
Fabrice Bacchella <fbacchella@spamcop.net>
2009-09-14 22:58:16 +0000

    junit update

--- junit/jrds/factories/TestConfigType.java
+++ junit/jrds/factories/TestConfigType.java
@@ -36,7 +36,7 @@ public class TestConfigType {
 	@Test
 	public void detectGraph() throws Exception {
 		Node n = Tools.parseRessource("customgraph.xml");
-		checkNode(n, ConfigType.GRAPH);
+		checkNode(n, ConfigType.GRAPHDESC);
 	}
 
 	@Test
