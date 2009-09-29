c88ff2b47e22ddb665e70e56082d5fc16cbb9d06
Fabrice Bacchella <fbacchella@spamcop.net>
2009-09-29 17:10:55 +0000

    Alpha numeric sort for the display tree

--- src/jrds/GraphTree.java
+++ src/jrds/GraphTree.java
@@ -6,8 +6,6 @@ _##########################################################################*/
 
 package jrds;
 
-import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
@@ -15,11 +13,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
-
-import jrds.webapp.ParamsBean;
 
 import org.apache.log4j.Logger;
 
@@ -34,11 +28,7 @@ public class GraphTree {
 	static public final int LEAF_HOSTNAME = 2;
 	static final private Logger logger = Logger.getLogger(GraphTree.class);
 
-	static final Comparator<String> nodeComparator = new Comparator<String>() {
-		public  int compare(String arg0, String arg1) {
-			return String.CASE_INSENSITIVE_ORDER.compare(arg0, arg1);
-		}
-	};
+	static final Comparator<String> nodeComparator = jrds.Util.AlphanumericSorting();
 
 	private GraphTree parent;
 	private Map<Integer, GraphTree> pathsMap;
@@ -54,7 +44,7 @@ public class GraphTree {
 	 *  
 	 */
 	private GraphTree(String name) {
-		graphsSet = new TreeMap<String, GraphNode>(String.CASE_INSENSITIVE_ORDER);
+		graphsSet = new TreeMap<String, GraphNode>(nodeComparator);
 		childsMap = new TreeMap<String, GraphTree>(nodeComparator);
 		this.name = name;
 	}
@@ -71,68 +61,9 @@ public class GraphTree {
 		return rootNode;
 	}
 
-	public boolean getJavaScriptCode(Writer out, ParamsBean params, String curNode, Filter f) throws IOException {
-
-		StringBuffer thisNode = new StringBuffer(1000);
-		thisNode.append(curNode + " = gFld('" + name.replaceAll("'","&#x27;") + "', '");
-		//thisNode.append(curNode + " = gFld('" + name.replaceAll("'","&#x27;") + "', 'index.jsp?id=" + getPath().hashCode());
-		//thisNode.append("&" + params);
-		thisNode.append(params.makeObjectUrl("index.jsp", getPath(), false));
-		thisNode.append("');\n");
-
-		StringBuffer childsarray = new StringBuffer(1000);
-		childsarray.append(curNode +".addChildren([\n");
-		int width = 0;
-		boolean first = true;
-		for(GraphTree o: childsMap.values()) {
-			String child = curNode + "_" + width++;
-			boolean noChild = o.getJavaScriptCode(out, params, child, f);
-			if(! noChild) {
-				if(! first)
-					childsarray.append(", ");
-				else {
-					childsarray.append("    ");
-				}
-				childsarray.append(child);
-				first = false;
-			}
-		}
-		for(Map.Entry<String, GraphNode> e: graphsSet.entrySet()) {
-			GraphNode currGraph = e.getValue();
-			String path = getPath() + "/" + currGraph.getName();
-			if(f == null || f.acceptGraph(currGraph, path)) {
-				//Start to render the accepted graph
-				if(! first)
-					childsarray.append(",\n");
-				first = false;
-
-				/* replace \ with \\
-				 * ex: C:\ become C:\\
-				 * */   
-				String leafName = e.getKey().replaceAll("\\\\","\\\\\\\\").replaceAll("'","&#x27;");
-				childsarray.append("    [");
-				childsarray.append("'" + leafName +"',");
-				childsarray.append("'");
-				childsarray.append(params.makeObjectUrl("index.jsp", currGraph, false));
-				//childsarray.append("index.jsp?id=");
-				//childsarray.append(currGraph.hashCode());
-				//childsarray.append("&" + params);
-				childsarray.append("']");
-			}
-		}
-		if(! first)
-			childsarray.append("\n");
-		childsarray.append("]);\n");
-
-		if(! first) {
-			out.append(thisNode);
-			out.append(childsarray);
-		}
-		return first;
-	}
-
 	public GraphTree getByPath(String path) {
-		logger.trace("Looking for path " + path + " with id " + path.hashCode());
+		if(logger.isTraceEnabled())
+			logger.trace("Looking for path " + path + " with id " + path.hashCode());
 		return getById(path.hashCode());
 	}
 
@@ -169,16 +100,8 @@ public class GraphTree {
 			_addGraphByPath(new LinkedList<String>(path), nodesGraph);
 	}
 
-	public Set<String> getChildsName() {
-		Set<String> retValue = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
-		for(String childName: childsMap.keySet()) {
-			retValue.add(childName);
-		}
-		return retValue;
-	}
-
 	public GraphTree getChildbyName(String name) {
-		return (GraphTree) childsMap.get(name);
+		return childsMap.get(name);
 	}
 
 	public String getName() {
--- src/jrds/Util.java
+++ src/jrds/Util.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.security.MessageDigest;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -399,6 +400,82 @@ public class Util {
 		return new Iterable<T>() {
 			public Iterator<T> iterator() {
 				return iterator;
+			}
+		};
+	}
+	
+	/**
+	 * Return an alpha numeric sorter where host2 is before host10
+	 * Copied from http://sanjaal.com/java/tag/sample-alphanumeric-sorting/
+	 * @return
+	 */
+	public static Comparator<String> AlphanumericSorting() {
+		return new Comparator<String>() {
+
+			public int compare(String firstString, String secondString) {
+
+				if (secondString == null || firstString == null) {
+					return 0;
+				}
+
+				int lengthFirstStr = firstString.length();
+				int lengthSecondStr = secondString.length();
+
+				int index1 = 0;
+				int index2 = 0;
+
+				while (index1 < lengthFirstStr && index2 < lengthSecondStr) {
+					char ch1 = firstString.charAt(index1);
+					char ch2 = secondString.charAt(index2);
+
+					char[] space1 = new char[lengthFirstStr];
+					char[] space2 = new char[lengthSecondStr];
+
+					int loc1 = 0;
+					int loc2 = 0;
+
+					do {
+						space1[loc1++] = ch1;
+						index1++;
+
+						if (index1 < lengthFirstStr) {
+							ch1 = firstString.charAt(index1);
+						} else {
+							break;
+						}
+					} while (Character.isDigit(ch1) == Character.isDigit(space1[0]));
+
+					do {
+						space2[loc2++] = ch2;
+						index2++;
+
+						if (index2 < lengthSecondStr) {
+							ch2 = secondString.charAt(index2);
+						} else {
+							break;
+						}
+					} while (Character.isDigit(ch2) == Character.isDigit(space2[0]));
+
+					String str1 = new String(space1);
+					String str2 = new String(space2);
+
+					int result;
+
+					if (Character.isDigit(space1[0]) && Character.isDigit(space2[0])) {
+						Integer firstNumberToCompare = new Integer(Integer
+								.parseInt(str1.trim()));
+						Integer secondNumberToCompare = new Integer(Integer
+								.parseInt(str2.trim()));
+						result = firstNumberToCompare.compareTo(secondNumberToCompare);
+					} else {
+						result = str1.compareTo(str2);
+					}
+
+					if (result != 0) {
+						return result;
+					}
+				}
+				return lengthFirstStr - lengthSecondStr;
 			}
 		};
 	}
