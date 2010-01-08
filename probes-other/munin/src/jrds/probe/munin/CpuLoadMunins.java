25bf002e266dc7a948cbcfe7f6af309d961b83ab
Fabrice Bacchella <fbacchella@spamcop.net>
2010-01-08 12:23:54 +0000

    munin update

---
+++ probes-other/munin/src/jrds/probe/munin/CpuLoadMunins.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 7 janv. 2005
+ *
+ * TODO
+ */
+package jrds.probe.munin;
+
+import org.rrd4j.DsType;
+
+import jrds.ProbeDesc;
+import jrds.RdsHost;
+
+/**
+ * @author bacchell
+ *
+ * TODO
+ */
+public class CpuLoadMunins extends Munin {
+	static final private ProbeDesc pd = new ProbeDesc(1);
+	static {
+		pd.add("la1", DsType.GAUGE, "load.value");
+		//pd.setMuninsProbesNames(new String[] { "load" });
+		pd.setProbeName("laveragemunins");
+
+		//pd.setGraphClasses(new Object[] {"cpuload"});
+	}
+}
---
+++ probes-other/munin/src/jrds/probe/munin/IfSolarisMunins.java
@@ -0,0 +1,30 @@
+/*
+ * Created on 7 janv. 2005
+ *
+ * TODO 
+ */
+package jrds.probe.munin;
+
+import org.rrd4j.DsType;
+
+import jrds.ProbeDesc;
+import jrds.RdsHost;
+
+/**
+ * @author bacchell
+ *
+ * TODO 
+ */
+public class IfSolarisMunins extends MuninIndexed {
+	static final private ProbeDesc pd = new ProbeDesc(5);
+	static {
+		pd.add("ifInOctets", DsType.COUNTER, "rbytes.value");
+		pd.add("ifOutOctets", DsType.COUNTER, "obytes.value");
+		pd.add("ifInErrors", DsType.COUNTER, "ierrors.value");
+		pd.add("ifOutErrors", DsType.COUNTER, "oerrors.value");
+		pd.add("collisions", DsType.COUNTER, "collisions.value");
+//		pd.setGraphClasses(new Object[] {"ifbps"});
+//		pd.setMuninsProbesNames(new String[] { "if_${index}", "if_errcoll_${index}"});
+		pd.setProbeName("if-_${index}_munins");
+	}
+}
