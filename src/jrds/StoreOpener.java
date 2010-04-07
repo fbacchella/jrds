b68f223c462a171978352cb2145c3694ffc221bf
Fabrice Bacchella <fbacchella@spamcop.net>
2010-04-07 12:27:21 +0000

    Updated Store Opener

--- src/jrds/StoreOpener.java
+++ src/jrds/StoreOpener.java
@@ -2,15 +2,13 @@ package jrds;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.log4j.Logger;
 import org.rrd4j.core.RrdBackendFactory;
 import org.rrd4j.core.RrdDb;
+import org.rrd4j.core.RrdDbPool;
 import org.rrd4j.core.RrdFileBackendFactory;
 
 /**
@@ -19,16 +17,7 @@ import org.rrd4j.core.RrdFileBackendFactory;
 public final class StoreOpener {
 	static final private Logger logger = Logger.getLogger(StoreOpener.class);
 
-	static private class Indirection {
-		RrdDb db = null;
-		int count = 0;
-	}
-
-	public static final int INITIAL_CAPACITY = 200;
-	private static Semaphore capacity = null;
-	private static int timeout;
-
-	static private final ConcurrentHashMap<String, Indirection> pool = new ConcurrentHashMap<String, Indirection>(INITIAL_CAPACITY);
+	private static RrdDbPool instance = null;
 
 	private static final AtomicLong waitTime = new AtomicLong(0);
 	private static final AtomicInteger lockCount = new AtomicInteger(0);
@@ -41,30 +30,16 @@ public final class StoreOpener {
 	 * @return RrdDb instance of the datasource.
 	 * @throws IOException Thrown in case of I/O error.
 	 * @throws InterruptedException 
-	 * @throws RrdException Thrown in case of a JRobin specific error.
 	 */
 	public final static RrdDb getRrd(String rrdFile)
 	throws IOException, InterruptedException {
 		File f = new File(rrdFile);
 		String cp = f.getCanonicalPath();
 		long start = System.currentTimeMillis();
-		lockCount.incrementAndGet();
-		Indirection ref = pool.putIfAbsent(cp, new Indirection());
-		if(ref == null)
-			ref = pool.get(cp);
-		synchronized(ref) {
-			if(ref.db == null) {
-				if(capacity.availablePermits() <= 0) {
-					logger.warn("Too much rrd opened, do you have a leak ?");
-				}
-				capacity.tryAcquire(timeout, TimeUnit.SECONDS);
-				ref.db = new RrdDb(rrdFile);
-			}
-			ref.count++;
-		}
+		RrdDb db = instance.requestRrdDb(cp);
 		long finish = System.currentTimeMillis();
 		waitTime.addAndGet(finish - start);
-		return ref.db;
+		return db;
 	}
 
 	/**
@@ -73,21 +48,7 @@ public final class StoreOpener {
 	public final static void releaseRrd(RrdDb arg0)  {
 		try {
 			long start = System.currentTimeMillis();
-			lockCount.incrementAndGet();
-			Indirection ref = pool.get(arg0.getCanonicalPath());
-			if(ref != null) {
-				synchronized(ref) {
-					if(ref.db != null) {
-						ref.count--;
-						if(ref.count <= 0) {
-							ref.db.close();
-							ref.db = null;
-							pool.remove(ref);
-							capacity.release();
-						}
-					}
-				}
-			}
+			instance.release(arg0);
 			long finish = System.currentTimeMillis();
 			waitTime.addAndGet(finish - start);
 		} catch (Exception e) {
@@ -96,8 +57,10 @@ public final class StoreOpener {
 	}
 
 	public static final void prepare(int dbPoolSize, int syncPeriod, int timeout, String backend) {
-		if(capacity == null) {
-			RrdBackendFactory.registerFactory(new RrdAccountingNioBackendFactory());
+		if(instance == null) {
+			instance = RrdDbPool.getInstance();
+			instance.setCapacity(dbPoolSize);
+			//RrdBackendFactory.registerFactory(new RrdAccountingNioBackendFactory());
 			RrdBackendFactory.setDefaultFactory(backend);
 		}
 		RrdBackendFactory factory = RrdBackendFactory.getDefaultFactory();
@@ -106,8 +69,6 @@ public final class StoreOpener {
 			throw new RuntimeException("Cannot create an opener with " +
 			"a default backend factory not derived from RrdFileBackendFactory");
 		}
-
-		capacity = new Semaphore(dbPoolSize);
 
 	}
 
