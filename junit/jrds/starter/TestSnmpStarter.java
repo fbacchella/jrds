e719b869917c9aa752b8b0ba3dc8b94cb56200ab
Fabrice Bacchella <fbacchella@spamcop.net>
2010-06-03 16:23:23 +0000

    junit updated

--- junit/jrds/starter/TestSnmpStarter.java
+++ junit/jrds/starter/TestSnmpStarter.java
@@ -1,6 +1,7 @@
 package jrds.starter;
 
 import jrds.Tools;
+import jrds.snmp.SnmpStarter;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -9,14 +10,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestSnmpStarter {
-	static Logger logger = Logger.getLogger(StarterTest.class);
+	static Logger logger = Logger.getLogger(TestSnmpStarter.class);
 
 	@BeforeClass
 	static public void configure() throws Exception {
 		Tools.configure();
 
 		logger.setLevel(Level.TRACE);
-		Tools.setLevel(new String[] {jrds.snmp.SnmpStarter.class.toString(), StarterNode.class.toString(), Starter.class.toString()}, logger.getLevel());
+		Tools.setLevel(new String[] {"org.snmp4j", "jrds.snmp", "jrds.starter"}, logger.getLevel());
 	}
 
 	@Test
@@ -24,9 +25,22 @@ public class TestSnmpStarter {
 		StarterNode n1 = new StarterNode() { };
 		n1.registerStarter(jrds.snmp.SnmpStarter.full);
 		
+		StarterNode n2 = new StarterNode(n1) { };
+		SnmpStarter snmp = new SnmpStarter();
+		snmp.setHostname("localhost");
+		n2.registerStarter(snmp);
+		
+		logger.debug("Starting at level 1");
 		n1.startCollect();
+		logger.debug("Starting at level 2");
+		n2.startCollect();
 
-		Assert.assertTrue(n1.isCollectRunning());
+		Assert.assertTrue(n2.isCollectRunning());
+
+		logger.debug("Stopping at level 1");
+		n1.stopCollect();
+
+		Assert.assertFalse(n2.isCollectRunning());
 
 	}
 }
