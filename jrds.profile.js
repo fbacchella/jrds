826915c3f7d5ee3bf523a8e55de138f0964c563d
Fabrice Bacchella <fbacchella@spamcop.net>
2010-09-30 19:07:53 +0000

    Set up a custom repository

--- jrds.profile.js
+++ jrds.profile.js
@@ -7,20 +7,21 @@ dependencies ={
             "dojo.parser",
             "dojo.data.ItemFileReadStore",
             "dijit.Dialog",
+            "dijit.TitlePane",
             "dijit.layout.BorderContainer",
             "dijit.layout.ContentPane",
             "dijit.layout.TabContainer",
+            "dijit.form.ValidationTextBox",
+            "dijit.form.TimeTextBox",
             "dijit.form.DateTextBox",
             "dijit.form.Button",
             "dijit.form.TextBox",
             "dijit.form.Select",
             "dijit.form.Form",
-            "dijit.form.ValidationTextBox",
             "dijit.form.NumberTextBox",
             "dijit.Tree",
-            "dijit.form.TimeTextBox",
             "dojox.widget.Standby",
-            "dijit.TitlePane"
+            "dojox.form.FileUploader"
         ]
         }
     ],
