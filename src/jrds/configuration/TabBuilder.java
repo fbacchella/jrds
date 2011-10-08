a391cc92592e3bf1177387af4bb649f6d91b0e25
Fabrice Bacchella <fbacchella@spamcop.net>
2011-10-08 17:52:11 +0200

    Tab don't use XPATH any more

--- src/jrds/configuration/TabBuilder.java
+++ src/jrds/configuration/TabBuilder.java
@@ -3,18 +3,13 @@ package jrds.configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.xpath.XPathExpression;
-
 import jrds.Tab;
-import jrds.factories.xml.CompiledXPath;
 import jrds.factories.xml.JrdsDocument;
 import jrds.factories.xml.JrdsElement;
 
 import org.apache.log4j.Logger;
 
 public class TabBuilder extends ConfigObjectBuilder<Tab>  {
-    static final private XPathExpression TABFILTER = CompiledXPath.get("/tab/filter");
-    static final private XPathExpression TABGRAPH = CompiledXPath.get("/tab/graph|/tab/cgraph");
 
     static final private Logger logger = Logger.getLogger(TabBuilder.class);
 
@@ -31,7 +26,7 @@ public class TabBuilder extends ConfigObjectBuilder<Tab>  {
             return null;
         }
         Tab tab;
-        if(n.checkPath(TABFILTER)) {
+        if(root.getElementbyName("filter") != null) {
             tab = new Tab.Filters(name);
             for(JrdsElement elemNode: root.getChildElementsByName("filter")) {
                 String elemName = elemNode.getTextContent();
@@ -40,7 +35,9 @@ public class TabBuilder extends ConfigObjectBuilder<Tab>  {
         }
         else {
             tab = new Tab.DynamicTree(name);
-            for(JrdsElement elemNode: n.iterate(TABGRAPH, JrdsElement.class)) {
+            for(JrdsElement elemNode: root.getChildElements() ) {
+                if(! "graph".equals(elemNode.getNodeName()) && !  "cgraph".equals(elemNode.getNodeName()))
+                    continue;
                 String id = elemNode.getAttribute("id");
                 if("cgraph".equals(elemNode.getNodeName()))
                     id = "/" + id;
