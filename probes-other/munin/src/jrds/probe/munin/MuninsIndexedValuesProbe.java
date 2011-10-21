1e481f1bb149377691b40720ec89246c2eea7b77
Fabrice Bacchella <fbacchella@spamcop.net>
2011-10-22 00:36:02 +0200

    Small cleaning in the munin probe

--- probes-other/munin/src/jrds/probe/munin/MuninsIndexedValuesProbe.java
+++ probes-other/munin/src/jrds/probe/munin/MuninsIndexedValuesProbe.java
@@ -1,5 +1,5 @@
 /*
- * Created on 8 févr. 2005
+ * Created on 8 fï¿½vr. 2005
  *
  * TODO 
  */
@@ -9,22 +9,26 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-
 /**
  * @author bacchell
  *
  * TODO 
  */
 public abstract class MuninsIndexedValuesProbe extends MuninIndexed {
-	public Map<String, Number> filterValues(Map<String, Number> valuesList)
-	{
-		Set<String> collected = getPd().getCollectStrings().keySet();
-		Map<String, Number> retValues = new HashMap<String, Number>(collected.size());
-		for(String collect: collected) {
-			Number value = valuesList.get(getIndexName() + "_" + collect);
-			if(value != null)
-				retValues.put(collect, value);
-		}
-		return retValues;
-	}
+    
+    /* (non-Javadoc)
+     * @see jrds.Probe#filterValues(java.util.Map)
+     */
+    @Override
+    public Map<String, Number> filterValues(Map<String, Number> valuesList)
+    {
+        Set<String> collected = getPd().getCollectStrings().keySet();
+        Map<String, Number> retValues = new HashMap<String, Number>(collected.size());
+        for(String collect: collected) {
+            Number value = valuesList.get(getIndexName() + "_" + collect);
+            if(value != null)
+                retValues.put(collect, value);
+        }
+        return retValues;
+    }
 }
