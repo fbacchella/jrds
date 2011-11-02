26d5c5563126aad3b31c8a25cdb4a9b6511173f4
Fabrice Bacchella <fbacchella@spamcop.net>
2011-11-02 14:55:30 +0100

    Ordered sync, to help the IO scheduler

--- junit/jrds/caching/TestPageCache.java
+++ junit/jrds/caching/TestPageCache.java
@@ -3,10 +3,12 @@ package jrds.caching;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import jrds.Tools;
+import jrds.Util;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -22,7 +24,7 @@ public class TestPageCache {
         Tools.configure();
         Tools.setLevel(logger, Level.TRACE, "jrds.caching.RrdCachedFileBackend", "jrds.caching.FilePage", "jrds.caching.PageCache");
     }
-    
+
     @Test
     public void test1() throws IOException {
         String outString = getClass().getName();
@@ -39,6 +41,29 @@ public class TestPageCache {
         in.read(b);
         Assert.assertEquals("read does not match write", outString.trim(), new String(b).trim());
         testFile.delete();
-}
+    }
+    
+    @Test
+    public void test2() throws IOException {
+        int numpages = 3;
+        PageCache pc = new PageCache(numpages,3600);
+        File testFile = new File("tmp/testpagecache");
+        if(testFile.exists())
+            testFile.delete();
+        byte[] buffer = new byte[PageCache.PAGESIZE];
+        for(byte i= 0; i < numpages; i++) {
+            Arrays.fill(buffer, i);
+            pc.write(testFile, i * PageCache.PAGESIZE, buffer);
+        }
+        pc.sync();
+        Assert.assertEquals("File size invalid", PageCache.PAGESIZE * numpages, testFile.length());
+        FileInputStream in = new FileInputStream(testFile);
+        byte[] bufferin = new byte[PageCache.PAGESIZE * numpages ];
+        in.read(bufferin);
+        for(int i=0 ; i < PageCache.PAGESIZE * numpages; i++ ) {
+            Assert.assertEquals(String.format("Invalid content at offset %d", i), (byte)Math.floor(i / PageCache.PAGESIZE), bufferin[i]);
+        }
+
+    }
 
 }
