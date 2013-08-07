b38975ec02b04482421f83614ec1c502b0d91655
Fabrice Bacchella <fbacchella@spamcop.net>
2013-08-07 22:29:22 +0200

    Added a big consistency check for all json rest queries

---
+++ junit/jrds/webapp/TestReadElements.java
@@ -0,0 +1,173 @@
+package jrds.webapp;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import jrds.PropertiesManager;
+import jrds.Tools;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mortbay.jetty.testing.HttpTester;
+import org.mortbay.jetty.testing.ServletTester;
+
+public class TestReadElements {
+    static final private Logger logger = Logger.getLogger(TestReadElements.class);
+
+    static ServletTester tester = null;
+    
+    private final class TreeContent {
+        List<JSONObject> graphs = new ArrayList<JSONObject>();
+        List<JSONObject> trees = new ArrayList<JSONObject>();
+        List<JSONObject> filters = new ArrayList<JSONObject>();
+    }
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @BeforeClass
+    static public void configure() throws Exception {
+        Tools.configure();
+        logger.setLevel(Level.TRACE);
+        System.setProperty("org.mortbay.log.class", "jrds.standalone.JettyLogger");
+        Tools.setLevel(logger.getLevel(), TestReadElements.class.getName(), "jrds.webapp");
+    }
+
+    @Before
+    public void launchServer() throws Exception {
+        URL configDirURL = Tools.class.getResource("/ressources/configfull/");
+        logger.debug(configDirURL.getFile());
+
+        Properties prop = new Properties();
+        prop.setProperty("strictparsing", "true");
+        prop.setProperty("readonly", "true");
+        prop.put("configdir", configDirURL.getFile());
+        prop.put("log.trace", "jrds.webapp");
+
+        tester = ToolsWebApp.getMonoServlet(testFolder, prop, JSonTree.class, "/jsontree");
+        tester.addServlet(JSonQueryParams.class, "/queryparams");
+        tester.addServlet(JSonGraph.class, "/jsongraph");
+        tester.start();        
+    }
+
+    private JSONObject jsonquery(String query) throws IOException, Exception {
+        HttpTester request = new HttpTester();
+        HttpTester response = new HttpTester();
+        request.setMethod("GET");
+        request.setHeader("Host","tester");
+        request.setHeader("Referer","http://tester/");
+        request.setURI(query);
+        request.setVersion("HTTP/1.0");
+
+        response.parse(tester.getResponses(request.generate()));
+        Assert.assertEquals(200, response.getStatus());
+        JSONObject content = new JSONObject(response.getContent());
+        logger.debug(content);
+
+        return(content);
+    }
+
+    private TreeContent scantree(JSONObject tree) throws IOException, Exception {
+        TreeContent result = new TreeContent();
+        int size = tree.getJSONArray("items").length();
+        List<JSONObject> graphs = new ArrayList<JSONObject>(size);
+        for(int i=0;i < size ; i++) {
+            JSONObject item = tree.getJSONArray("items").getJSONObject(i);
+            String type = item.getString("type");
+            if("graph".equals(type)) {
+                String id = item.getString("id").split("\\.")[1];
+                JSONObject graphdetails = jsonquery("/jsongraph?id=" + id); 
+                result.graphs.add(graphdetails);
+            }
+            else if("tree".equals(type)) {
+                result.trees.add(item);
+            }
+            else if("filter".equals(type)) {
+                result.filters.add(item);
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void testQueryHost() throws IOException, Exception {
+        JSONObject tree = jsonquery("/jsontree?host=localhost");
+        logger.trace(tree);
+        Assert.assertEquals(4, tree.getJSONArray("items").length());   
+        Assert.assertEquals(3,  scantree(tree).graphs.size());
+        Assert.assertEquals(1,  scantree(tree).trees.size());
+        Assert.assertEquals(0,  scantree(tree).filters.size());
+    }
+
+    @Test
+    public void testServicesTab() throws IOException, Exception {
+        JSONObject tree = jsonquery("/jsontree?tab=" + PropertiesManager.SERVICESTAB);
+        logger.trace(tree);
+        Assert.assertEquals(0, tree.getJSONArray("items").length());
+        Assert.assertEquals(0,  scantree(tree).graphs.size());
+        Assert.assertEquals(0,  scantree(tree).trees.size());
+        Assert.assertEquals(0,  scantree(tree).filters.size());
+    }
+
+    @Test
+    public void testViewTab() throws IOException, Exception {
+        JSONObject tree = jsonquery("/jsontree?tab=" + PropertiesManager.VIEWSTAB);
+        logger.trace(tree);
+        Assert.assertEquals(11, tree.getJSONArray("items").length());        
+        Assert.assertEquals(3,  scantree(tree).graphs.size());
+        Assert.assertEquals(1,  scantree(tree).trees.size());
+        Assert.assertEquals(0,  scantree(tree).filters.size());
+    }
+
+    @Test
+    public void testHostTab() throws IOException, Exception {
+        JSONObject tree = jsonquery("/jsontree?tab=" + PropertiesManager.HOSTSTAB);
+        logger.trace(tree);
+        Assert.assertEquals(4, tree.getJSONArray("items").length());        
+        Assert.assertEquals(3,  scantree(tree).graphs.size());
+        Assert.assertEquals(1,  scantree(tree).trees.size());
+        Assert.assertEquals(0,  scantree(tree).filters.size());
+    }
+
+    @Test
+    public void testTagsTab() throws IOException, Exception {
+        JSONObject tree = jsonquery("/jsontree?tab=" + PropertiesManager.TAGSTAB);
+        logger.trace(tree);
+        Assert.assertEquals(1, tree.getJSONArray("items").length());        
+        Assert.assertEquals(0,  scantree(tree).graphs.size());
+        Assert.assertEquals(0,  scantree(tree).trees.size());
+        Assert.assertEquals(1,  scantree(tree).filters.size());
+    }
+
+    @Test
+    public void testTabList() throws IOException, Exception {
+        JSONObject queryparams = jsonquery("/queryparams");
+        JSONObject tabslist = queryparams.getJSONObject("tabslist");
+        for(String key: new String[]{ PropertiesManager.FILTERTAB, 
+                PropertiesManager.SUMSTAB,
+                PropertiesManager.SERVICESTAB,
+                PropertiesManager.VIEWSTAB,
+                PropertiesManager.HOSTSTAB,
+                PropertiesManager.TAGSTAB,
+                PropertiesManager.ADMINTAB,
+                PropertiesManager.CUSTOMGRAPHTAB}) {
+            JSONObject jsontab = tabslist.getJSONObject(key);
+            Assert.assertNotEquals(null, jsontab);
+            Assert.assertNotEquals(null, jsontab.get("id"));
+            Assert.assertNotEquals(null, jsontab.get("callback"));
+            Assert.assertNotEquals(null, jsontab.get("isFilters"));
+            Assert.assertNotEquals(null, jsontab.get("label"));
+        }
+    }
+
+}
