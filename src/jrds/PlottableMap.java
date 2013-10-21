823c639e7aed4455988c10b6bab3ce826c157c9f
Fabrice Bacchella <fbacchella@spamcop.net>
2013-10-21 16:03:25 +0200

    Reformated PlottableMap

--- src/jrds/PlottableMap.java
+++ src/jrds/PlottableMap.java
@@ -4,22 +4,23 @@ import java.util.HashMap;
 
 import org.rrd4j.data.Plottable;
 
+@SuppressWarnings("serial")
 public abstract class PlottableMap extends HashMap<String, Plottable>{
-	public static class ProxyPlottable extends Plottable {
-		Plottable real = new Plottable() {};
-		@Override
-		public double getValue(long timestamp) {
-			return real.getValue(timestamp);
-		}
-		public void setReal(Plottable real) {
-			this.real = real;
-		}
-	}
-	/**
-	 * Fill the map with the appropriate Plottable, for the given time span specification
-	 * @param start the start time, in second
-	 * @param end the end time, in second
-	 * @param step the step, in second
-	 */
-	public abstract void configure(long start, long end, long step);
+    public static class ProxyPlottable extends Plottable {
+        Plottable real = new Plottable() {};
+        @Override
+        public double getValue(long timestamp) {
+            return real.getValue(timestamp);
+        }
+        public void setReal(Plottable real) {
+            this.real = real;
+        }
+    }
+    /**
+     * Fill the map with the appropriate Plottable, for the given time span specification
+     * @param start the start time, in second
+     * @param end the end time, in second
+     * @param step the step, in second
+     */
+    public abstract void configure(long start, long end, long step);
 }
