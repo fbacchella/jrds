044143feef5797c8cdf0132d03838a82ed66fad2
Fabrice Bacchella <fbacchella@spamcop.net>
2014-05-20 23:34:16 +0200

    Auto detect JMX and activate beans if needed. Added a getLastValues() to the jrds' mbean

--- src/jrds/jmx/ManagementMBean.java
+++ src/jrds/jmx/ManagementMBean.java
@@ -1,8 +1,11 @@
 package jrds.jmx;
 
+import java.util.Map;
+
 public interface ManagementMBean {
     public void reload();
     public int getHostsCount();
     public int getProbesCount();
     public int getGeneration();
+    public Map<String, Number> getLastValues(String host, String probeName);
 }
