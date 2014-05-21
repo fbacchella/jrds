89d82bf7faf5f94d9124675d179029e7f926b345
Fabrice Bacchella <fbacchella@spamcop.net>
2014-05-21 12:58:44 +0200

    For HP servers: updated ribcl and added RibclHTP for ilo 3 and more

--- src/jrds/probe/Ribcl.java
+++ src/jrds/probe/Ribcl.java
@@ -8,13 +8,10 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.TransformerException;
 
@@ -22,6 +19,7 @@ import jrds.Probe;
 import jrds.Util;
 import jrds.factories.ProbeBean;
 import jrds.factories.ProbeMeta;
+import jrds.starter.SSLStarter;
 import jrds.starter.SocketFactory;
 import jrds.starter.XmlProvider;
 
@@ -30,171 +28,161 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 @ProbeMeta(
-        topStarter=jrds.starter.XmlProvider.class
+        topStarter = jrds.starter.XmlProvider.class
         )
-@ProbeBean({"user", "password", "iloHost"})
-public class Ribcl extends Probe<String, Number> {
-	private String user;
-	private String passwd;
-	private String iloHost;
-	private Integer port = 443;
+@ProbeBean({"user", "password", "iloHost", "port"})
+public class Ribcl extends Probe<String, Number> implements SSLProbe {
+    private String user;
+    private String passwd;
+    private String iloHost;
+    private Integer port = 443;
 
-	static final private String encoding = "ISO-8859-1";
-	static final private String eol = "\r\n";
-	static final private String xmlHeader = "<?xml version=\"1.0\" ?>" + eol;
+    static final private String encoding = "ISO-8859-1";
+    static final private String eol = "\r\n";
+    static final private String xmlHeader = "<?xml version=\"1.0\" ?>" + eol;
 
-	public void configure(String iloHost, int port, String user, String passwd) {
-		this.iloHost = iloHost;
-		this.user = user;
-		this.passwd = passwd;
-		this.port = port;
-		
-	}
+    public void configure(String iloHost, int port, String user, String passwd) {
+        this.iloHost = iloHost;
+        this.user = user;
+        this.passwd = passwd;
+        this.port = port;
+    }
 
-	public void configure(String iloHost, String user, String passwd) {
-		this.iloHost = iloHost;
-		this.user = user;
-		this.passwd = passwd;
-	}
+    public void configure(String iloHost, String user, String passwd) {
+        this.iloHost = iloHost;
+        this.user = user;
+        this.passwd = passwd;
+    }
 
-	@Override
-	public Map<String, Number> getNewSampleValues() {
-		Map<String, Number> vars = new HashMap<String, Number>();
-		Socket s = null;
-		try {
-			s = connect();
-		} catch (Exception e) {
-			log(Level.ERROR, e, "SSL connect error %s", e);
-			return null;
-		}
+    public void configure() {
+    }
 
-		try {
-			XmlProvider xmlstarter  = find(XmlProvider.class);
-			if(xmlstarter == null) {
-				log(Level.ERROR, "XML Provider not found");
-				return null;
-			}
-			
-			if(! isCollectRunning())
-				return null;
+    @Override
+    public Map<String, Number> getNewSampleValues() {
+        Socket s = null;
+        try {
+            s = connect();
+        } catch (Exception e) {
+            log(Level.ERROR, e, "SSL connect error %s", e);
+            return Collections.emptyMap();
+        }
 
-			OutputStream outputSocket = s.getOutputStream();
-			InputStream inputSocket = s.getInputStream();
+        XmlProvider xmlstarter  = find(XmlProvider.class);
+        if(xmlstarter == null) {
+            log(Level.ERROR, "XML Provider not found");
+            return Collections.emptyMap();
+        }
 
-			outputSocket.write(xmlHeader.getBytes(encoding));
-			buildQuery(outputSocket, xmlstarter);
+        if(! isCollectRunning())
+            return Collections.emptyMap();
 
-			byte[] buffer = new byte[4096];
-			int n;
-			StringBuffer message = null;
-			while((n = inputSocket.read(buffer)) > 0) {
-				String messageBuffer = new String(buffer, 0, n, encoding);
-				if(messageBuffer.startsWith("<?xml version=\"1.0\"?>")) {
-					if(message != null)
-						parse(message.toString(), vars, xmlstarter);
-					message = new StringBuffer(messageBuffer);
-				}
-				else {
-					message.append(messageBuffer);
-				}
-			}
-			s.close();
-		} catch (IOException e) {
-			log(Level.ERROR, e, "SSL socket error %s", e);
-		}
+        Map<String, Number> vars = new HashMap<String, Number>();
 
-		return vars;
-	}
+        try {
+            OutputStream outputSocket = s.getOutputStream();
+            InputStream inputSocket = s.getInputStream();
 
-	@Override
-	public String getSourceType() {
-		return "RIBCL";
-	}
+            outputSocket.write(xmlHeader.getBytes(encoding));
+            buildQuery(outputSocket, xmlstarter);
 
-	private void buildQuery(OutputStream out, XmlProvider xmlstarter) {
-		Document ribclQ = xmlstarter.getDocument();
-		Element LOCFG = ribclQ.createElement("LOCFG");
-		LOCFG.setAttribute("version", "2.21");
-		ribclQ.appendChild(LOCFG);
-		Element RIBCL = ribclQ.createElement("RIBCL");
-		LOCFG.appendChild(RIBCL);
-		RIBCL.setAttribute("VERSION", "2.0");
-		Element LOGIN = ribclQ.createElement("LOGIN");
-		LOGIN.setAttribute("USER_LOGIN", user);
-		LOGIN.setAttribute("PASSWORD", passwd);
-		RIBCL.appendChild(LOGIN);
-		Element command = ribclQ.createElement(getPd().getSpecific("command"));
-		command.setAttribute("MODE", "read");
-		LOGIN.appendChild(command);
-		Element subcommand = ribclQ.createElement(getPd().getSpecific("subcommand"));
-		command.appendChild(subcommand);
+            byte[] buffer = new byte[4096];
+            int n;
+            StringBuffer message = null;
+            while((n = inputSocket.read(buffer)) > 0) {
+                String messageBuffer = new String(buffer, 0, n, encoding);
+                log(Level.TRACE, "message content: %s", messageBuffer);
+                if(messageBuffer.startsWith("<?xml version=\"1.0\"?>")) {
+                    if(message != null)
+                        parse(message.toString(), vars, xmlstarter);
+                    message = new StringBuffer(messageBuffer);
+                }
+                else if(message != null){
+                    message.append(messageBuffer);
+                }
+                else {
+                    log(Level.ERROR, "invalid response from %s: %s", iloHost, messageBuffer);
+                    break;
+                }
+            }
+            s.close();
+        } catch (IOException e) {
+            log(Level.ERROR, e, "SSL socket error %s", e);
+        }
 
-		Map<String, String> properties = new HashMap<String, String>();
-		properties.put(OutputKeys.INDENT, "no");
-		properties.put(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		try {
-			Util.serialize(ribclQ, out, null, properties);
-		} catch (TransformerException e) {
-		} catch (IOException e) {
-			log(Level.FATAL, e, "Unable to serialize in memory");
-			throw new Error(e);
-		}
-	}
+        return vars;
+    }
 
-	private Socket connect() throws NoSuchAlgorithmException, KeyManagementException, UnknownHostException, IOException {
-		SocketFactory ss = find(SocketFactory.class); 
-		Socket s = ss.createSocket(iloHost, port);
-		if (s == null)
-			return s;
-		
-		if(port == 23) {
-			return 	s;
-		}		
-		// Create a trust manager that does not validate certificate chains
-		TrustManager[] trustAllCerts = new TrustManager[]{
-				new X509TrustManager() {
-					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-						return null;
-					}
-					public void checkClientTrusted(
-							java.security.cert.X509Certificate[] certs, String authType) {
-					}
-					public void checkServerTrusted(
-							java.security.cert.X509Certificate[] certs, String authType) {
-					}
-				}
-		};
+    @Override
+    public String getSourceType() {
+        return "RIBCL";
+    }
 
-		SSLContext sc = SSLContext.getInstance("SSL");
-		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+    protected Document makeDocument(XmlProvider xmlstarter) {
+        Document ribclQ = xmlstarter.getDocument();
+        Element LOCFG = ribclQ.createElement("LOCFG");
+        LOCFG.setAttribute("version", "2.21");
+        ribclQ.appendChild(LOCFG);
+        Element RIBCL = ribclQ.createElement("RIBCL");
+        LOCFG.appendChild(RIBCL);
+        RIBCL.setAttribute("VERSION", "2.0");
+        Element LOGIN = ribclQ.createElement("LOGIN");
+        LOGIN.setAttribute("USER_LOGIN", user);
+        LOGIN.setAttribute("PASSWORD", passwd);
+        RIBCL.appendChild(LOGIN);
+        Element command = ribclQ.createElement(getPd().getSpecific("command"));
+        command.setAttribute("MODE", "read");
+        LOGIN.appendChild(command);
+        Element subcommand = ribclQ.createElement(getPd().getSpecific("subcommand"));
+        command.appendChild(subcommand);
+        return ribclQ;
+    }
 
-		SSLSocketFactory ssf = sc.getSocketFactory();
-		if(! isCollectRunning())
-			return null;
-		s = ssf.createSocket(s, iloHost, port, true);
-		log(Level.DEBUG, "done SSL handshake for %s", iloHost);
-		return s;
-	}
+    protected void buildQuery(OutputStream out, XmlProvider xmlstarter) {
+        Document ribclQ = makeDocument(xmlstarter);
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put(OutputKeys.INDENT, "no");
+        properties.put(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        properties.put(OutputKeys.ENCODING, encoding);
+        try {
+            Util.serialize(ribclQ, out, null, properties);
+        } catch (TransformerException e) {
+            log(Level.FATAL, e, "Unable to serialize in memory");
+            throw new Error(e);
+        } catch (IOException e) {
+            log(Level.FATAL, e, "Unable to serialize in memory");
+            throw new Error(e);
+        }
+    }
 
-	public void parse(String message, Map<String, Number> vars, XmlProvider xmlstarter) {
-		if(message == null ||  "".equals(message))
-			return;
-		log(Level.TRACE,"new message to parse: ");
-		log(Level.TRACE, message);
-		//The XML returned from an iLO is buggy, up to ilO2 1.50 
-		message = message.replaceAll("<RIBCL VERSION=\"[0-9\\.]+\"/>", "<RIBCL >");
-		Document d = xmlstarter.getDocument(new StringReader(message));
-		xmlstarter.fileFromXpaths(d, getPd().getCollectStrings().keySet(), vars);
-		return;
-	}
+    private Socket connect() throws NoSuchAlgorithmException, KeyManagementException, UnknownHostException, IOException {
+        if(port == 23) {
+            SocketFactory ss = find(SocketFactory.class); 
+            Socket s = ss.createSocket(iloHost, port);
+            return s;
+        }               
 
-	/* (non-Javadoc)
-	 * @see jrds.starter.StarterNode#isStarted(java.lang.Object)
-	 */
-	@Override
-	public boolean isStarted(Object key) {
-		return super.isStarted(key) && find(XmlProvider.class).isStarted() && find(SocketFactory.class).isStarted();
-	}
+        return find(SSLStarter.class).connect(iloHost, port);
+    }
+
+    public void parse(String message, Map<String, Number> vars, XmlProvider xmlstarter) {
+        if(message == null ||  "".equals(message))
+            return;
+        log(Level.TRACE,"new message to parse: ");
+        log(Level.TRACE, message);
+        //The XML returned from an iLO is buggy, up to ilO2 1.50 
+        message = message.replaceAll("<RIBCL VERSION=\"[0-9\\.]+\"/>", "<RIBCL >");
+        Document d = xmlstarter.getDocument(new StringReader(message));
+        xmlstarter.fileFromXpaths(d, getPd().getCollectStrings().keySet(), vars);
+        return;
+    }
+
+    /* (non-Javadoc)
+     * @see jrds.starter.StarterNode#isStarted(java.lang.Object)
+     */
+    @Override
+    public boolean isStarted(Object key) {
+        return super.isStarted(key) && find(XmlProvider.class).isStarted() && find(SocketFactory.class).isStarted();
+    }
 
     /**
      * @return the user
