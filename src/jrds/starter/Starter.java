f41c9dad352d402a4a5c4674a748be5a49a4574b
Fabrice Bacchella <fbacchella@spamcop.net>
2015-02-26 14:55:19 +0100

    Some reformating

--- src/jrds/starter/Starter.java
+++ src/jrds/starter/Starter.java
@@ -8,25 +8,25 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public abstract class Starter {
-	long uptime = Long.MAX_VALUE;
+    long uptime = Long.MAX_VALUE;
 
-	private StarterNode level;	
-	private Logger namedLogger = null;
-	volatile private boolean started = false;
+    private StarterNode level;	
+    private Logger namedLogger = null;
+    volatile private boolean started = false;
 
-	public Starter() {
-		String[] classElements = getClass().getName().split("\\.");
-		namedLogger = Logger.getLogger("jrds.Starter."  + classElements[classElements.length - 1 ].replaceAll("\\$", ".\\$"));
-	}
+    public Starter() {
+        String[] classElements = getClass().getName().split("\\.");
+        namedLogger = Logger.getLogger("jrds.Starter."  + classElements[classElements.length - 1 ].replaceAll("\\$", ".\\$"));
+    }
 
     /**
-	 * This method is called when the started is really registred<p>
-	 * It can be overriden to contains delayed initialization but it must begin with a call to super.initialize(parent)
-	 * @param level
-	 */
-	public void initialize(StarterNode level) {
-		this.level = level;
-	}
+     * This method is called when the started is really registred<p>
+     * It can be overriden to contains delayed initialization but it must begin with a call to super.initialize(parent)
+     * @param level
+     */
+    public void initialize(StarterNode level) {
+        this.level = level;
+    }
 
     /**
      * It's called after the starter registration but in host list configuration
@@ -39,90 +39,90 @@ public abstract class Starter {
     }
 
     public final void doStart() {
-		log(Level.TRACE, "Starting");
-		try {
-			long begin = new Date().getTime();
-			started = start();
-			long end = new Date().getTime();
-			log(Level.DEBUG, "Starting connection took %d ms", end - begin);
-		} catch (Exception e) {
-			log(Level.ERROR, e, "Error while starting: %s", e.getMessage());
+        log(Level.TRACE, "Starting");
+        try {
+            long begin = new Date().getTime();
+            started = start();
+            long end = new Date().getTime();
+            log(Level.DEBUG, "Starting connection took %d ms", end - begin);
+        } catch (Exception e) {
+            log(Level.ERROR, e, "Error while starting: %s", e.getMessage());
         } catch (NoClassDefFoundError e) {
-            log(Level.ERROR, e, "Class %s not found", e.getMessage().replace('/', '.'));
-		}
-	}
+            log(Level.ERROR, e, e.getMessage().replace('/', '.'));
+        }
+    }
 
-	public final void doStop() {
-		if(started) {
-			log(Level.TRACE, "Stopping");
-			started = false;
-			stop();
-		}
-	}
-	
-	public boolean start() {
-		return true;
-	}
+    public final void doStop() {
+        if(started) {
+            log(Level.TRACE, "Stopping");
+            started = false;
+            stop();
+        }
+    }
 
-	public void stop() {
-	}
+    public boolean start() {
+        return true;
+    }
 
-	public Object getKey() {
-		return getClass().getName();
-	}
+    public void stop() {
+    }
 
-	public StarterNode getLevel() {
-		return level;
-	}
+    public Object getKey() {
+        return getClass().getName();
+    }
 
-	public boolean isStarted() {
-		return started;
-	}
+    public StarterNode getLevel() {
+        return level;
+    }
 
-	@Override
-	public String toString() {
-		String levelString = "''";
-		String keyString = "";
-		if(level != null)
-			levelString = level.toString();
-		Object key = getKey();
-		if(key instanceof Class<?>) {
-			keyString = ((Class<?>) key).getName();
-		}
-		else {
-			keyString = key.toString();
-		}
-		return keyString + "@" + levelString;
-	}
+    public boolean isStarted() {
+        return started;
+    }
 
-	/**
-	 * Return uptime for the starter, default value is max value
-	 * @return
-	 */
-	public long getUptime() {
-		return uptime;
-	}
+    @Override
+    public String toString() {
+        String levelString = "''";
+        String keyString = "";
+        if(level != null)
+            levelString = level.toString();
+        Object key = getKey();
+        if(key instanceof Class<?>) {
+            keyString = ((Class<?>) key).getName();
+        }
+        else {
+            keyString = key.toString();
+        }
+        return keyString + "@" + levelString;
+    }
 
-	public void setUptime(long uptime) {
-		this.uptime = uptime;
-	}
+    /**
+     * Return uptime for the starter, default value is max value
+     * @return
+     */
+    public long getUptime() {
+        return uptime;
+    }
 
-	/**
-	 * @deprecated
-	 * Use getLevel instead.
-	 * @return the StarterNode for this starter
-	 */
-	@Deprecated
-	public StartersSet getParent() {
-		return level;
-	}
+    public void setUptime(long uptime) {
+        this.uptime = uptime;
+    }
 
-	public void log(Level l, Throwable e, String format, Object... elements) {
-		jrds.Util.log(this, namedLogger, l, e, format, elements);
-	}
+    /**
+     * @deprecated
+     * Use getLevel instead.
+     * @return the StarterNode for this starter
+     */
+    @Deprecated
+    public StartersSet getParent() {
+        return level;
+    }
 
-	public void log(Level l, String format, Object... elements) {
-		jrds.Util.log(this, namedLogger,l, null, format, elements);
-	}
+    public void log(Level l, Throwable e, String format, Object... elements) {
+        jrds.Util.log(this, namedLogger, l, e, format, elements);
+    }
+
+    public void log(Level l, String format, Object... elements) {
+        jrds.Util.log(this, namedLogger,l, null, format, elements);
+    }
 
 }
