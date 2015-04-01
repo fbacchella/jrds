f5be01de52d2cddf53c760dc7c13854ceec98d14
Anthony Pessy <anthony.pessy@gmail.com>
2015-04-01 19:18:42 +0200

    remove unused imports

--- src/jrds/GraphTree.java
+++ src/jrds/GraphTree.java
@@ -8,7 +8,6 @@ package jrds;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
--- src/jrds/configuration/ConfigObjectBuilder.java
+++ src/jrds/configuration/ConfigObjectBuilder.java
@@ -1,6 +1,5 @@
 package jrds.configuration;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
