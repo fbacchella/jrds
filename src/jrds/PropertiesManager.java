2854e07d458895f321c842b732f4f9b5494673c3
Anthony Pessy <anthony.pessy@gmail.com>
2015-04-02 19:24:44 +0200

    unnecessary check for autocreate, always true

--- src/jrds/PropertiesManager.java
+++ src/jrds/PropertiesManager.java
@@ -219,7 +219,7 @@ public class PropertiesManager extends Properties {
                 logger.error(dir + " doesn't exists");
                 return null;
             }
-            if ( autocreate && !dir.mkdirs()) {
+            if (!dir.mkdirs()) {
                 logger.error(dir + " doesn't exists and can't be created");
                 return null;
             }
