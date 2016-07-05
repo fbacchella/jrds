43482a2db884af9671a0b2cf6f6d37a9f44062f8
Fabrice Bacchella <fbacchella@spamcop.net>
2016-07-05 12:55:57 +0200

    Munin was not stripping comments. Slightly improved the probe.

--- src/jrds/probe/munin/Munin.java
+++ src/jrds/probe/munin/Munin.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import jrds.ProbeConnected;
 import jrds.factories.ProbeMeta;
@@ -17,6 +18,9 @@ import org.apache.log4j.Level;
 @ProbeMeta(discoverAgent = MuninDiscoverAgent.class)
 public class Munin extends ProbeConnected<String, Number, MuninConnection> {
 
+    private static final Pattern COMMENT = Pattern.compile("#.*");
+    private static final Pattern VALUESUFFIX = Pattern.compile(".value");
+
     public Munin() {
         super(MuninConnection.class.getName());
     }
@@ -27,7 +31,7 @@ public class Munin extends ProbeConnected<String, Number, MuninConnection> {
 
         Map<String, Number> retValue = new HashMap<String, Number>();
         String fetchList = getPd().getSpecific("fetch");
-        if(fetchList == null) {
+        if (fetchList == null) {
             log(Level.ERROR, "no fetch list defined");
             return Collections.emptyMap();
         }
@@ -38,21 +42,22 @@ public class Munin extends ProbeConnected<String, Number, MuninConnection> {
                 String lastLine;
                 boolean dotFound = false;
                 while(! dotFound && ( lastLine = muninSocket.in.readLine()) != null ) {
-                    lastLine.replaceFirst("#.*", "");
-                    if(".".equals(lastLine)) {
+                    lastLine = COMMENT.matcher(lastLine).replaceFirst("");
+                    if (".".equals(lastLine)) {
                         dotFound = true;
-                    }
-                    else {
+                    } else {
                         String[] kvp = lastLine.split(" ");
-                        if(kvp.length == 2) {
+                        if (kvp.length == 2) {
                             String name = kvp[0];
                             Number value = jrds.Util.parseStringNumber(kvp[1], Double.NaN);
-                            if(name != null && value != null)
-                                retValue.put(name.replace(".value", ""), value);
+                            if (name != null && value != null) {
+                                String valueName = VALUESUFFIX.matcher(name).replaceFirst("");
+                                retValue.put(valueName, value);
+                            }
                         }
                     }
                 }
-                if(! dotFound) {
+                if (! dotFound) {
                     log(Level.WARN, "Munin connection finished early");
                 }
             }
