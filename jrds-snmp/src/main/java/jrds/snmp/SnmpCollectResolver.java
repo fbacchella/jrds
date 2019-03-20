dabcc2b4d12efe27fea1aaca1f74aa1632788a9e
Fabrice Bacchella <fbacchella@spamcop.net>
2019-03-20 11:31:35 +0100

    Correction of snmp probes with new probe desc builder.

--- jrds-snmp/src/main/java/jrds/snmp/SnmpCollectResolver.java
+++ jrds-snmp/src/main/java/jrds/snmp/SnmpCollectResolver.java
@@ -6,9 +6,19 @@ import jrds.CollectResolver;
 
 public class SnmpCollectResolver implements CollectResolver<OID> {
 
+    private static final OID NULLOID = new OID(new int[] {0,0});
+
     @Override
     public OID resolve(String collectKey) {
-        return new OID(collectKey);
+        try {
+            OID collect = new OID(collectKey);
+            if (NULLOID.equals(collect) || ! collect.isValid()) {
+                throw new IllegalArgumentException("Resolved as invalid: '" + collect + "'");
+            }
+            return collect;
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
     }
 
 }
