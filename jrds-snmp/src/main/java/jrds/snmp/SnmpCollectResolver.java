b4be7265dd33cfae277df235884f2836330dd1e7
Fabrice Bacchella <fbacchella@spamcop.net>
2019-06-27 12:35:14 +0200

    Improved SNMP resolver.

--- jrds-snmp/src/main/java/jrds/snmp/SnmpCollectResolver.java
+++ jrds-snmp/src/main/java/jrds/snmp/SnmpCollectResolver.java
@@ -1,14 +1,15 @@
 package jrds.snmp;
 
 import java.lang.invoke.MethodHandles;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snmp4j.smi.OID;
 
 import jrds.CollectResolver;
+import jrds.Util;
 
 public class SnmpCollectResolver implements CollectResolver<OID> {
 
@@ -19,27 +20,18 @@ public class SnmpCollectResolver implements CollectResolver<OID> {
     /**
      * An OID mapping cache, filled in {@link SnmpConfigurator#configure(jrds.PropertiesManager)}
      */
-    static final Map<String, OID> oidmapping = new HashMap<>();
+    static final Map<String, OID> oidmapping = new ConcurrentHashMap<>();
 
     @Override
     public OID resolve(String collectKey) {
-        try {
-            if (oidmapping.containsKey(collectKey)) {
-                return oidmapping.get(collectKey);
-            } else {
-                OID collect = new OID(collectKey);
-                if (NULLOID.equals(collect) || ! collect.isValid()) {
-                    throw new IllegalArgumentException("Resolved as invalid: '" + collect + "'");
-                }
-                oidmapping.put(collectKey, collect);
-                if (logger.isDebugEnabled()) {
-                    logger.debug(String.format("Missing from OID mappings: %s=%s", collectKey, collect.toDottedString()));
-                }
-                return collect;
+        return oidmapping.computeIfAbsent(collectKey, k -> {
+            OID collect = new OID(collectKey);
+            if (NULLOID.equals(collect) || ! collect.isValid()) {
+                throw new IllegalArgumentException("Resolved as invalid: '" + collectKey + "'");
             }
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e.getMessage(), e);
-        }
+            logger.debug("Missing from OID mappings: {}={}", collectKey, Util.delayedFormatString(collect::toDottedString));
+            return collect;
+        });
     }
 
 }
