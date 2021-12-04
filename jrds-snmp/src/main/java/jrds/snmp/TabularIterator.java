8f38c77fc449995e75f66560fcb66fbc8c5b2cb9
Fabrice Bacchella <fbacchella@spamcop.net>
2021-12-04 17:21:34 +0100

    Sonar: An Iterable should not be it's own iterator.

--- jrds-snmp/src/main/java/jrds/snmp/TabularIterator.java
+++ jrds-snmp/src/main/java/jrds/snmp/TabularIterator.java
@@ -1,7 +1,9 @@
 package jrds.snmp;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import org.snmp4j.Target;
 import org.snmp4j.smi.OID;
@@ -10,43 +12,45 @@ import org.snmp4j.util.DefaultPDUFactory;
 import org.snmp4j.util.TableEvent;
 import org.snmp4j.util.TableUtils;
 
-public class TabularIterator implements Iterator<SnmpVars>, Iterable<SnmpVars> {
-    Iterator<TableEvent> tabIterator;
+public class TabularIterator implements Iterable<SnmpVars> {
+
+    private final List<TableEvent> events;
 
     public TabularIterator(SnmpConnection starter, Collection<OID> oids) {
-        if(starter != null && starter.isStarted()) {
+        if (starter != null && starter.isStarted()) {
             Target snmpTarget = starter.getConnection();
-            if(snmpTarget != null) {
+            if (snmpTarget != null) {
                 DefaultPDUFactory localfactory = new DefaultPDUFactory();
                 TableUtils tableRet = new TableUtils(starter.getSnmp(), localfactory);
                 tableRet.setMaxNumColumnsPerPDU(30);
                 OID[] oidTab = new OID[oids.size()];
                 oids.toArray(oidTab);
-                tabIterator = tableRet.getTable(snmpTarget, oidTab, null, null).iterator();
+                events = tableRet.getTable(snmpTarget, oidTab, null, null);
+            } else {
+                events = Collections.emptyList();
             }
+        } else {
+            events = Collections.emptyList();
         }
     }
 
-    public boolean hasNext() {
-        return tabIterator.hasNext();
-    }
-
-    public SnmpVars next() {
-        TableEvent te = tabIterator.next();
-        VariableBinding[] columns = te.getColumns();
-        SnmpVars vars;
-        if(columns != null)
-            vars = new SnmpVars(columns);
-        else
-            vars = new SnmpVars();
-        return vars;
-    }
-
-    public void remove() {
-        throw new UnsupportedOperationException("Cannot remove in a TabularIterator");
-    }
-
+    @Override
     public Iterator<SnmpVars> iterator() {
-        return this;
+        Iterator<TableEvent> tabIterator = events.iterator();
+        return new Iterator<SnmpVars>() {
+
+            @Override
+            public boolean hasNext() {
+                return tabIterator.hasNext();
+            }
+
+            @Override
+            public SnmpVars next() {
+                TableEvent te = tabIterator.next();
+                VariableBinding[] columns = te.getColumns();
+                return columns != null ? new SnmpVars(columns) : new SnmpVars();
+            }
+        };
     }
+
 }
